803b2430a00e08e35c429abdb3ea184a1c0e9be6
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-04-08 14:06:55 +0300

    Update kooperatifmarket.py

--- plugins/kooperatifmarket.py
+++ plugins/kooperatifmarket.py
@@ -1,3 +1,22 @@
 from pyrogram import Client, filters
 import requests
 from bs4 import BeautifulSoup
+import logging 
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+                    level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+butonlar = InlineKeyboardMarkup([[InlineKeyboardButton(f'Bim Gelecek Hafta Salı', callback_data='bimges')]])
+
+@Client.on_message(filters.command('koop'))
+async def koop(bot, message):
+    try:
+        url = "https://www.akakce.com/brosurler/kooperatifmarket"
+        i = requests.get(url)
+        corba = BeautifulSoup(i.content, "lxml")
+        LOGGER.info(corba)
+    except Exception as e:
+        await message.reply_text(e)
