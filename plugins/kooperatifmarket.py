b4441c0060ba33d012b6fa98aca2504e5f88eef2
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-04-08 16:53:44 +0300

    Update kooperatifmarket.py

--- plugins/kooperatifmarket.py
+++ plugins/kooperatifmarket.py
@@ -11,11 +11,16 @@ LOGGER = logging.getLogger(__name__)
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 butonlar = InlineKeyboardMarkup([[InlineKeyboardButton(f'Bim Gelecek Hafta Salı', callback_data='bimges')]])
 
+ac = requests.Session()
+headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+        }
+
 @Client.on_message(filters.command('dizigom'))
 async def dizigom(bot, message):
     try:
         url = message.text.split(" ")[1]
-        i = requests.get(url)
+        i = ac.get(url, headers=headers)
         corba = BeautifulSoup(i.content, "lxml")
         LOGGER.info(corba)
     except Exception as e:
