ebf4f0c71e842d927f27a3de2166ed206904fb2a
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-04-08 18:07:38 +0300

    Update dizigom.py

--- plugins/dizigom.py
+++ plugins/dizigom.py
@@ -18,7 +18,7 @@ headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
         }
 
-browser = webdriver.Chrome()
+browser = webdriver.Firefox()
 
 @Client.on_message(filters.command('dizigom'))
 async def dizigom(bot, message):
