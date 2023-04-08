e69bcdfbedf854a6cae94c8ddaf535bc9e3888c1
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-04-08 18:11:20 +0300

    Update dizigom.py

--- plugins/dizigom.py
+++ plugins/dizigom.py
@@ -13,17 +13,11 @@ LOGGER = logging.getLogger(__name__)
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 butonlar = InlineKeyboardMarkup([[InlineKeyboardButton(f'Bim Gelecek Hafta Salı', callback_data='bimges')]])
 
-ac = requests.Session()
-headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
-        }
-
-browser = webdriver.Firefox()
-
 @Client.on_message(filters.command('dizigom'))
 async def dizigom(bot, message):
     try:
         url = message.text.split(" ")[1]
+        browser = webdriver.Firefox()
         i = browser.get(url) 
         embedtemp = i.text.split('"contentUrl":"')[1]
         embed = embedtemp.split('"')[0]
