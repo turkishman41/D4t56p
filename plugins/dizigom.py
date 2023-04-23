3eeb84d6190157bf2b60911fee31a137f75455c8
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-04-23 13:08:59 +0300

    Update dizigom.py

--- plugins/dizigom.py
+++ plugins/dizigom.py
@@ -13,13 +13,18 @@ LOGGER = logging.getLogger(__name__)
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 butonlar = InlineKeyboardMarkup([[InlineKeyboardButton(f'Bim Gelecek Hafta Salı', callback_data='bimges')]])
 
-@Client.on_message(filters.command('dizigom'))
-async def dizigom(bot, message):
+@Client.on_message(filters.command('fox'))
+async def fox(bot, message):
     try:
         url = message.text.split(" ")[1]
         istek = requests.get(url) 
         corba = BeautifulSoup(istek.content, "lxml")
         LOGGER.info(corba)
-        await message.reply_text(istek.url)
+        if 'videoSrc' in istek.content:
+            temp = istek.content.split("videoSrc : '")[1]
+            url = temp.split("'")[0]
+            await message.reply_text(url)
+        else:
+            await message.reply_text("m3u8 Alamadım :(")
     except Exception as e:
         await message.reply_text(e)
