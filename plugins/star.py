1f6ff270cdf7793c6dea997026e037b4708f40f0
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-04-23 22:11:27 +0300

    Update star.py

--- plugins/star.py
+++ plugins/star.py
@@ -18,21 +18,10 @@ async def star(bot, message):
             url = mes[1]
             say = int(mes[2])
             sayi = say + 1
-            r = requests.get(url)
-            LOGGER.info(r.content)
-            if '\rvar videoDataList' in r.text:
-                temp = r.text.split('\rvar videoDataList')[1]
-                urtemp = temp.split('"videoUrl": "')[1]
-                ur = urtemp.split('"')[0]
-                idtemp = temp.split('"m3u8VideoId": "')[1]
-                id = idtemp.split('"')[0]
-                url1 = f"{ur}&m3u8VideoId={id}&totalPartCount=1"
-                await message.reply_text(url1)
             for bolum in range(1, sayi):
                 bolsay = url.split("/")[7]
                 uri = url.replace(str(bolsay), f"{bolum}-bolum") 
                 r = requests.get(uri) 
-                LOGGER.info(r.content)
                 if '\rvar videoDataList' in r.text:
                     temp = r.text.split('\rvar videoDataList')[1]
                     urtemp = temp.split('"videoUrl": "')[1]
@@ -40,7 +29,11 @@ async def star(bot, message):
                     idtemp = temp.split('"m3u8VideoId": "')[1]
                     id = idtemp.split('"')[0]
                     url1 = f"{ur}&m3u8VideoId={id}&totalPartCount=1"
-                    await message.reply_text(url1)
+                    jsontemp = requests.get(url1)
+                    json = json.load(jsontemp.content)
+                    LOGGER.info(json)
+                    await message.reply_text(url1) 
+                    await message.reply_text(json)
                 else:
                     await message.reply_text(f"{bolum}. için m3u8 Alamadım :(")
     except Exception as e:
