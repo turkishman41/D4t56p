6cb10f5aa5f9a42b645e172229424270a4d99f26
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-04-28 19:31:56 +0300

    Create sayi.py

---
+++ plugins/sayi.py
@@ -0,0 +1,32 @@
+from pyrogram import Client, filters
+from pyromod import listen
+import random
+
+
+
+async def sayitahmin(bot, message, secilensayi, kere):
+    tahmin = await bot.ask(message.chat.id, "Merhaba Dostum 1-100 arası tuttuğum sayıyı bul bakalım..")
+    kere += 1
+    tahmingetir(bot, message, tahmin, secilensayi, kere)
+    
+async def tahmingetir(bot, message, tahmin, secilensayi, kere):
+    try:
+        sayisi = int(tahmin)
+        if sayisi == int(secilensayi):
+            await bot.send_message(message.chat.id ) 
+    except:
+        sohbetediliyor(bot, message, secilensayi)
+        
+async def sohbetediliyor(bot, message, secilensayi, kere):
+    tahmin = await bot.ask(message.chat.id, "Lütfen Oyun Oynanırken Sohbet Etmeyin 😡")
+    kere +=1
+    tahmingetir(bot, message, tahmin, secilensayi)
+    
+@Client.on_message(filters.command('sayi'))
+async def sayioyunu(bot, message):
+    try:
+        secilensayi = random.randint(1, 100)
+        kere = 0
+        sayitahmin(bot, message, kere, secilensayi)
+    except Exception as e:
+        await message.reply_text(e)
