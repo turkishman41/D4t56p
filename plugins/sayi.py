3d88787930ba89c2b4393868a1eed84041c89d59
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-04-28 21:18:01 +0300

    Update sayi.py

--- plugins/sayi.py
+++ plugins/sayi.py
@@ -1,30 +1,30 @@
+# :d tarafından mamaklı için yazılmıştır.
+# Sayı Tahmin Oyunu
 from pyrogram import Client, filters
 from pyromod import listen
 import random
-# :d
-
 
 async def sayitahmin(bot, message, secilensayi, kere):
-    tahmin = await bot.ask(message.chat.id, "Merhaba Dostum 1-100 arası tuttuğum sayıyı bul bakalım..")
-    kere += 1
-    await tahmingetir(bot, message, tahmin, secilensayi, kere)
+    tahmin = await bot.ask(message.chat.id, "Merhaba Dostum 1-100 arası tuttuğum sayıyı bul bakalım..") # Oyuncudan Sayı istendi..
+    kere += 1 # kere'ye +1 ekletildi çünkü yukarda bir tahmin edildi.
+    await tahmingetir(bot, message, tahmin, secilensayi, kere) # Tahmin edilen sayıyı seçilen sayıyla karşılaştırmak için bir fonksiyon yazıldı.
     
 async def tahmingetir(bot, message, tahmin, secilensayi, kere):
     try:
-        text = tahmin.text
-        if not text.isdigit():
-            await sohbetediliyor(bot, message, secilensayi, kere) 
-        elif int(text) == int(secilensayi):
-            await bot.send_message(message.chat.id, f"Tebrikler {kere} deneyişte Buldun") 
-        elif int(text) > int(secilensayi):
+        text = tahmin.text # Tahmin texti alındı
+        if not text.isdigit(): # Sayı mı Kelime mi kontrol edildi. 
+            await sohbetediliyor(bot, message, secilensayi, kere) # Kelimeyse tekrar tahmin mesajı gönderildi. 
+        elif int(text) == int(secilensayi): # tahmin edilen sayı seçilen sayıyla aynı mı diye kontrol edildi. 
+            await bot.send_message(message.chat.id, f"Tebrikler Doğru Cevap: {secilensayi}\n{kere} deneyişte Buldun") # Doğru bilme mesajı attırıldı..
+        elif int(text) > int(secilensayi): # tahmin edilen seçilenden büyük mü diye kontrol edildi.
             await buyuktahmin(bot, message, secilensayi, kere)
-        elif int(text) < int(secilensayi):
+        elif int(text) < int(secilensayi): # Tahmin edilen seçilen sayıdan küçük mü kontrol edildi. 
             await kucuktahmin(bot, message, secilensayi, kere)
 
     except Exception as e:
         await message.reply_text(e)
 async def sohbetediliyor(bot, message, secilensayi, kere):
-    tahmin = await bot.ask(message.chat.id, "Lütfen Oyun Oynanırken Sohbet Etmeyin 😡")
+    tahmin = await bot.ask(message.chat.id, "Lütfen Oyun Oynanırken Sohbet Etmeyin 😡") # Yeni bir tahmin mesajı uyatı biçiminde gönderildi.
     kere +=1
     await tahmingetir(bot, message, tahmin, secilensayi, kere)
     
@@ -34,27 +34,15 @@ async def buyuktahmin(bot, message, secilensayi, kere):
     await tahmingetir(bot, message, tahmin, secilensayi, kere)
 
 async def kucuktahmin(bot, message, secilensayi, kere):
-
     tahmin = await bot.ask(message.chat.id, "az Söyledin yukarı çık")
-
     kere +=1
-
     await tahmingetir(bot, message, tahmin, secilensayi, kere)
 
- 
-    
-
-    
-
-   
- 
-    
-    
 @Client.on_message(filters.command('sayi'))
 async def sayioyun(bot, message):
     try:
-        secilensayi = random.randint(1, 100)
-        kere = 0
-        await sayitahmin(bot, message, secilensayi, kere)
+        secilensayi = random.randint(1, 100) # 1 ile 100 arasında sayı seçtiriliyor.
+        kere = 0 # kaç kere de bilindiğini yazdırmak için int olarak bri değişken oluşturuldu
+        await sayitahmin(bot, message, secilensayi, kere) # Sayı Tahmin döngüsü bailatıldı.
     except Exception as e:
         await message.reply_text(e)
