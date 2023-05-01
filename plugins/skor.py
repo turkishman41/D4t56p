be463ee45555a609cdcf2060608ed1100a0995bf
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-05-01 14:15:16 +0300

    Update skor.py

--- plugins/skor.py
+++ plugins/skor.py
@@ -16,10 +16,10 @@ async def skorlar(bot, message):
     corbam = BeautifulSoup(istek.content, "lxml")
     jsontemp = istek.text.split("jsonData = [")[1]
     jsonum = jsontemp.split("]")[0]
-    text = json.dumps(jsonum)
+    text = dict(jsonum)
     
-    await message.reply_text(f"{text[1]}")
-    textjs = json.loads(jsonum)
+    await message.reply_text(text)
+    textjs = json.loads(text)
     await message.reply_text(f"{textjs['tournamentName']}")
     LOGGER.info(text)
     
