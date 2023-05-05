3e790df093ae2e7c1bd18fc36927a3d16cae7d35
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-05-05 21:10:06 +0300

    Update diziyou.py

--- plugins/diziyou.py
+++ plugins/diziyou.py
@@ -12,31 +12,31 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from bs4 import BeautifulSoup
 
 async def diziyoum3u8getir(bot, message, url, say):
-        for bolum in range(1, say):
-            urtemp = url.split("sezon-")[1]
-            gitcek = urtemp.split("-")[0]
-            uri = url.replace(f"{gitcek}", f"{bolum}")
-            r = requests.get(uri)
-            corba = BeautifulSoup(r.content, "lxml") 
-             htmllinktemp = corba.find('iframe', attrs={"id":"diziyouPlayer"})
-            htmllink = htmllinktemp.get("src")
-            isimtemp = r.text.split("fas fa-play-circle")[1]
-            isim1 = r.text.split("<")[1]
-            isim = r.text.split("<")[0]  
-            m3u8istek = requests.get(htmllink)
-            m3u8corba = BeautifulSoup(m3u8istek.content, "lxml")
-            texttemp = m3u8corba.find('source', attrs={"id":"diziyouSource"})
-            text = texttemp.get("src")
-            altyazilar = m3u8corba.findAll('track', attrs={"kind":"captions"})
-            altmes = ""
-            for a in altyazilar:
-                alturl = a.get('src')
-                altdil = a.get('label')
-                altmes += f"Dil: {altdil}\nUrl: {alturl}\n\n"
-            await message.reply_text(altmes)
-            await message.reply_text(f"{text} | {isim}.mp4")
-            doc = "plugins/star.py"
-            await message.reply_document(doc)
+    for bolum in range(1, say):
+        urtemp = url.split("sezon-")[1]
+        gitcek = urtemp.split("-")[0]
+        uri = url.replace(f"{gitcek}", f"{bolum}")
+        r = requests.get(uri)
+        corba = BeautifulSoup(r.content, "lxml") 
+        htmllinktemp = corba.find('iframe', attrs={"id":"diziyouPlayer"})
+        htmllink = htmllinktemp.get("src")
+        isimtemp = r.text.split("fas fa-play-circle")[1]
+        isim1 = r.text.split("<")[1]
+        isim = r.text.split("<")[0]  
+        m3u8istek = requests.get(htmllink)
+        m3u8corba = BeautifulSoup(m3u8istek.content, "lxml")
+        texttemp = m3u8corba.find('source', attrs={"id":"diziyouSource"})
+        text = texttemp.get("src")
+        altyazilar = m3u8corba.findAll('track', attrs={"kind":"captions"})
+        altmes = ""
+        for a in altyazilar:
+            alturl = a.get('src')
+            altdil = a.get('label')
+            altmes += f"Dil: {altdil}\nUrl: {alturl}\n\n"
+        await message.reply_text(altmes)
+        await message.reply_text(f"{text} | {isim}.mp4")
+        doc = "plugins/star.py"
+        await message.reply_document(doc)
 
 @Client.on_message(filters.command('deneme'))
 async def denemeurl(bot, message):
