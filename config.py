d819c1d788f8be9a97a69ce4c625263921a951b9
turkishman41 <89349479+turkishman41@users.noreply.github.com>
2023-05-08 03:26:56 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-API_HASH = os.environ.get("API_HASH", None)
-API_ID = os.environ.get("API_ID", None)
-OWNER_ID = os.environ.get("OWNER_ID", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "65175278119:AAEqVHswUmPzcOWz-bfU5I4MavQP9ykIRLI")
+API_HASH = os.environ.get("API_HASH", "e5kf7a25123dca9d734516cde8ae4c374")
+API_ID = os.environ.get("API_ID", "1318481")
+OWNER_ID = os.environ.get("OWNER_ID", "hh312")
